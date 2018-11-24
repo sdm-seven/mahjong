a1b58e6e9e726995a774f4d31edd5d781eea2775
sdm_seven <793652351@qq.com>
2018-11-24 22:28:52 +0800

    init

---
+++ mahjong/pages/index/order.vue
@@ -0,0 +1,99 @@
+<template>
+	<view class="content">
+		<view class="section">		  
+		  <view class="flex-wrp" style="height: 300px;flex-direction:column;">
+			  <view v-for="(order,index) in orders" :key ="index">										  
+					<view class="section__title">{{order.orderNo}}<navigator class='a' url="">【置顶】</navigator></view>
+					<view class="flex-wrp" style="height: 300px;flex-direction:column;">
+						<view class="flex-item">订单发布者：{{order.publisher}} <navigator url="" class="pull-r">申请维修并投送简历</navigator></view>
+						<view class="flex-item">{{order.title}}</view>
+						<view class="flex-item">大概地址：{{order.address}}</view>
+						<view class="flex-item">详细地址：{{order.detailAdd}}</view>
+						<view class="flex-item">电话：{{order.tel}}</view>
+						<view class="flex-item">订单发布日期：{{order.publishTime}}</view>
+					</view>
+				</view>
+		  </view>
+		</view>
+	</view>	
+</template>
+
+<script>
+	export default {
+		data: {
+			orders:[{
+				orderNo:'1',				
+				title:'1',
+				address:'',
+				publisher:'',
+				publishTime:'',
+				detailAdd:'',
+				tel:''
+			},{
+				orderNo:'2',				
+				title:'2',
+				address:'',
+				publisher:'',
+				publishTime:'',
+				detailAdd:'',
+				tel:''
+			}]
+		}
+	}
+</script>
+
+<style>
+	.content {
+		flex: 1;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+	
+	.box{
+	    box-shadow: 0 0.01rem 0.1rem #ececec;
+	    background-color: #fff;
+	    margin-bottom: 0.3rem;
+	    border-radius: 0.1rem;
+	    position: relative;
+	}
+	
+	.box .h2{
+	    font-weight: 400;
+	    color: #999999;
+	    font-size: 0.26rem;
+	    padding: 0.15rem 0.2rem 0.1rem;
+	    border-bottom: 1px dotted #e4e4e4;
+	}
+	.box .h2 .a{
+	    color: #f68e8e;
+	    font-size: 0.28rem;
+	    float: right;
+	}
+	.box .ul{
+	    padding: 0.2rem;
+	}
+	.box .ul .li{
+	    margin-bottom: 0.2em;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</style>
---
+++ mahjong/unpackage/dist/build/app-plus/mahjong/pages/index/index.js
@@ -0,0 +1,13 @@
+
+      !(function(){
+        var uniAppViewReadyCallback = function(){
+          setCssToHead([".",[1],"content{-webkit-box-flex:1;-webkit-flex:1;flex:1;-webkit-box-pack:center;-webkit-justify-content:center;justify-content:center;-webkit-box-align:center;-webkit-align-items:center;align-items:center}\n.",[1],"title{font-size:",[0,36],";color:#8f8f94}\n.",[1],"box{box-shadow:0 ",[0,1]," ",[0,10]," #ececec;background-color:#fff;margin-bottom:",[0,30],";border-radius:",[0,10],";position:relative}\n.",[1],"box .",[1],"h2{font-weight:400;color:#999;font-size:",[0,26],";padding:",[0,15]," ",[0,20]," ",[0,10],";border-bottom:1px dotted #e4e4e4}\n.",[1],"box .",[1],"h2 .",[1],"a{color:#f68e8e;font-size:",[0,28.000000000000004],";float:right}\n.",[1],"box .",[1],"ul{padding:",[0,20],"}\n.",[1],"box .",[1],"ul .",[1],"li{margin-bottom:.2em}\n",],undefined,{path:"./pages/index/index.wxss"})();
+document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/index/index.wxml') } }));
+        }
+        if(window.__uniAppViewReady__){
+          uniAppViewReadyCallback()
+        }else{
+          document.addEventListener('uniAppViewReady',uniAppViewReadyCallback)
+        }
+      })();
+      
